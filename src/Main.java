1f4005da28d2d708ad1dafb7a497e8fb4568558f
Dmitrii Kosachev <dkosachev@t1.ru>
2025-08-09 15:30:31 +0300

    added operations with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        // Блок кода, отвечающий за получение данных от пользователя
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt(); // объявление переменной Второе число
+
+        // Блок кода, отвечаюший за вычисление операций
+        int sum = firstNumber + secondNumber; // операция сложения
+        int difference = firstNumber - secondNumber; // операция вычитания
+        int multiplication = firstNumber * secondNumber; // операция умножения
+        double quotient = (double) firstNumber / secondNumber; // операция деления
+
+        // Блок кода, отвечающий за вывод значений операций
+        System.out.println("Сумма двух чисел: " + sum);
+        System.out.println("Разность двух чисел: " + difference);
+        System.out.println("Произведение двух чисел: " + multiplication);
+        System.out.println("Частное двух чисел: " + quotient);
     }
 }
